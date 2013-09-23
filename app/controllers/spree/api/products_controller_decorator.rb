ae20edc4050be1961a4ddc9c45f99778a6bde380
Oleg Tsarenko <o.tsarenko@111min.com>
2013-09-23 16:41:13 +0300

    Add functionality for get categories with nested categories

---
+++ app/controllers/spree/api/products_controller_decorator.rb
@@ -0,0 +1,14 @@
+
+Spree::Api::ProductsController.class_eval do
+  def index
+    if params[:ids]
+      @products = product_scope.where(:id => params[:ids])
+    elsif params[:show_in_taxon] && params[:q].present? && params[:q][:taxon_id].present?
+      @products = product_scope.in_taxon(Spree::Taxon.find(params[:q][:taxon_id])).ransack(params[:q]).result
+    else
+      @products = product_scope.ransack(params[:q]).result
+    end
+
+    @products = @products.page(params[:page]).per(params[:per_page])
+  end
+end
