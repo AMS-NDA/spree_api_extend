ed84eaea922b2ee68e41ba3a70a3c8a3116421a3
Oleg Tsarenko <o.tsarenko@111min.com>
2013-09-30 12:21:37 +0300

    Add cart api, change user create

--- app/controllers/spree/api/carts_controller.rb
+++ app/controllers/spree/api/carts_controller.rb
@@ -4,6 +4,10 @@ module Spree
       def index
         if current_api_user
           @order = Spree::Order.where(user_id: current_api_user.id, :state => 'cart').first
+          unless @order
+            @order = Order.build_from_api(current_api_user, {})
+          end
+
           render :template => 'spree/api/orders/show'
         else
           unauthorized
--- app/controllers/spree/api/users_controller_decorator.rb
+++ app/controllers/spree/api/users_controller_decorator.rb
@@ -2,12 +2,10 @@
 # This No Bueno ?
 Spree::Api::UsersController.class_eval do
   def create
-    @user = Spree.user_class.new(params[:user])
-    if @user.generate_spree_api_key!
-      # # Create session for user?
-      # sign_in(@user, bypass: true)
-      # session[:spree_user_signup] = true
-      # associate_user
+    authorize! :create, Spree.user_class
+    @user = Spree.user_class.new(user_params)
+    if @user.save
+      @user.generate_spree_api_key!
       respond_with(@user, :status => 201, :default_template => :show)
     else
       invalid_resource!(@user)
