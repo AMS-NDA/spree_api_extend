7b22809a03c0d28245fa916f429c4ba22cd4bc14
Oleg Tsarenko <o.tsarenko@111min.com>
2013-10-15 13:24:52 +0300

    Fix doubling product in time search

--- app/controllers/spree/api/products_controller_decorator.rb
+++ app/controllers/spree/api/products_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::Api::ProductsController.class_eval do
     elsif params[:show_in_taxon] && params[:q].present? && params[:q][:taxon_id].present?
       @products = product_scope.in_taxon(Spree::Taxon.find(params[:q][:taxon_id])).ransack(params[:q]).result
     else
-      @products = product_scope.ransack(params[:q]).result
+      @products = product_scope.ransack(params[:q]).result.distinct
     end
 
     @products = @products.page(params[:page]).per(params[:per_page])
